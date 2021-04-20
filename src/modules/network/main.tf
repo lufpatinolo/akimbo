f68104e1247384a74e7e57cbb87f2e9b6dc598d6
lufpatinolo <lfpatinol@correo.udsistrital.edu.co>
2021-04-20 18:06:11 -0500

    :zap: update network

--- src/modules/network/main.tf
+++ src/modules/network/main.tf
@@ -17,16 +17,6 @@ resource "aws_subnet" "private_subnet1" {
   }
 }
 
-resource "aws_subnet" "private_subnet2" {
-  vpc_id            = aws_vpc.vpc_main.id
-  cidr_block        = var.private_subnet2_cidr
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "private subnet 2"
-  }
-}  
-
 resource "aws_subnet" "public_subnet1" {
   vpc_id            = aws_vpc.vpc_main.id
   cidr_block        = var.public_subnet1_cidr
@@ -37,12 +27,28 @@ resource "aws_subnet" "public_subnet1" {
   }
 }
 
-resource "aws_subnet" "public_subnet2" {
-  vpc_id            = aws_vpc.vpc_main.id
-  cidr_block        = var.public_subnet2_cidr
-  availability_zone = "us-east-1b"
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc_main.id
 
   tags = {
-    Name = "public subnet 2"
+    "Name" = "Internet Gateway main"
   }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc_main.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    "Name" = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_table_1" {
+  subnet_id      = aws_subnet.public_subnet1.id
+  route_table_id = aws_route_table.public_rt.id
 }
