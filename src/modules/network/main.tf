6dfa8eb7cbf6bfbf2ee3177e57bec51218a8efbb
lufpatinolo <lfpatinol@correo.udsistrital.edu.co>
2021-04-20 19:00:50 -0500

    :rocket: create nat_gw

--- src/modules/network/main.tf
+++ src/modules/network/main.tf
@@ -52,3 +52,38 @@ resource "aws_route_table_association" "public_table_1" {
   subnet_id      = aws_subnet.public_subnet1.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_eip" "eip" {
+  vpc = true
+
+  tags = {
+    "Name" = "Elastic IP for NAT gw"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public_subnet1.id  
+
+  tags = {
+    "Name" = "Nat Gateway"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc_main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    "Name" = "Private Route Table"
+  }
+}
+
+resource "aws_route_table_association" "private_table_1" {
+  subnet_id = aws_subnet.private_subnet1.id
+  route_table_id = aws_route_table.private_rt.id
+}
