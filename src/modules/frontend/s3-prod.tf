30f9e9eee68ba250fb04697d5faa86cd7ed9f9a3
lufpatinolo <lfpatinol@correo.udsistrital.edu.co>
2021-05-06 13:27:56 -0500

    :rocket: Create s3 fbd Prod

---
+++ src/modules/frontend/s3-prod.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "frontend-static-fbd-prod" {
+  bucket = "fundacion-bolivar-davivienda-prod"
+  acl    = "private"
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3-fbd-block-prod" {
+    bucket                  = aws_s3_bucket.frontend-static-fbd-prod.id
+    ignore_public_acls      = true
+    block_public_policy     = true
+    block_public_acls       = true
+    restrict_public_buckets = true
+    
+}
+
+resource "aws_s3_bucket_policy" "s3-fbd-policy-prod" {
+  bucket = aws_s3_bucket.frontend-static-fbd-prod.id
+  policy = jsonencode({
+    "Version": "2008-10-17",
+    "Id": "PolicyForCloudFrontPrivateContent",
+    "Statement": [
+        {
+            "Sid": "1",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E2EMT9YG4D2LYN"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::fundacion-bolivar-davivienda-prod/*"
+        }
+    ]
+})
+}
